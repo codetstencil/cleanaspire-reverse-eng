f0f46a26debe52270bfd56308a611c84becb9408
Fadahunsi Oluwaseyi <oluwaseyisamuel018@gmail.com>
2025-06-11 09:36:15 +0100

    add customer entity with migration (#1)

---
+++ src/CleanAspire.Domain/Entities/Customer.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CleanAspire.Domain.Common;
+
+namespace CleanAspire.Domain.Entities;
+public class Customer : BaseAuditableEntity, IAuditTrial
+{
+    /// <summary>
+    /// Gets or sets the name of the customer.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+    /// <summary>
+    /// Gets or sets the email of the customer.
+    /// </summary>
+    public string Email { get; set; } = string.Empty;
+    /// <summary>
+    /// Gets or sets the phone number of the customer.
+    /// </summary>
+    public string PhoneNumber { get; set; } = string.Empty;
+    /// <summary>
+    /// Gets or sets the address of the customer.
+    /// </summary>
+    public string Address { get; set; } = string.Empty;
+}
